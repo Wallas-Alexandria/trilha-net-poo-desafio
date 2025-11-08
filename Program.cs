7e57c05afaf590724d75f845bcfb34d6577103ca
Wallas Alexandria <alexandria.wallas@gmail.com>
2025-11-08 13:20:13 -0300

    Foi implementado as propriedades e chamado os metodos

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone("123-456-7890", "iPhone 13", "IMEI1234567890", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
+
+
+
+Nokia nokia = new Nokia("098-765-4321", "Nokia 3310", "IMEI0987654321", 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Snake");
